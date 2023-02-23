38ecf4ab9cc6f90c948dc1d2bcc3ee515d96d0a0
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-02-23 12:04:59 +0900

    feat : 이진 트리 구현

---
+++ leetcode/BinaryTree.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value = 0, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        
+bt = BinaryTree
+bt.root = Node(value=1)
+bt.root.left = Node(value=2)
+bt.root.right = Node(value=3)
+bt.root.left.left = Node(value=4)
+bt.root.left.right = Node(value=4)
+bt.root.right.left = Node(value=5)
+
