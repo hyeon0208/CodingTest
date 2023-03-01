82902247d7a465c8fe8dc41110b97d144208cd70
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-03-01 19:49:38 +0900

    feat : 피로도 (백트래킹)

---
+++ programmers/lv2/피로도.py
@@ -0,0 +1,20 @@
+answer = 0
+visited = []
+
+def solution(k, dungeons):
+    dun_num = len(dungeons)
+    global visited
+    visited = [False] * len(dungeons)
+    
+    def dfs(k, dungeons, depth):
+        global answer
+        answer = max(answer, depth)
+
+        for i in range(len(dungeons)):
+            if not visited[i] and dungeons[i][0] <= k:
+                visited[i] = True
+                dfs(k - dungeons[i][1], dungeons, depth + 1)
+                visited[i] = False
+    dfs(k, dungeons, 0)
+    
+    return answer
