42da7f69327d3fb5304ae16dbb43110103759e82
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-04-11 13:29:02 +0900

    feat : 달리기 경주

---
+++ programmers/lv2/달리기경주.py
@@ -0,0 +1,17 @@
+def solution(players, callings):
+    rankDic = {}
+    playerDic = {}
+
+    for idx, player in enumerate(players):
+        rankDic[idx + 1] = player
+        playerDic[player] = idx + 1
+    
+    for cur_player in callings:
+        cur_rank = playerDic[cur_player]
+        prev_rank = cur_rank - 1
+        prev_player = rankDic[prev_rank]
+
+        rankDic[prev_rank], rankDic[cur_rank] = rankDic[cur_rank], rankDic[prev_rank]
+        playerDic[prev_player], playerDic[cur_player] = playerDic[cur_player], playerDic[prev_player]
+
+    return list(rankDic.values())
