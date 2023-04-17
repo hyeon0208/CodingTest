9252df18a30135732d3cdac338304bbe39fc90d3
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-04-17 12:28:52 +0900

    feat : 광물캐기

---
+++ programmers/lv2/광물캐기.py
@@ -0,0 +1,39 @@
+def solution(picks, minerals):
+    sum = 0
+    for x in picks:
+        sum += x
+    
+    mnr = sum * 5 # 캘 수 있는 광물의 개수
+    if len(minerals) > mnr: # 주어진 광물이 캘 수 있는 광물 수보다 크면
+        minerals = minerals[:mnr]
+        
+    cnt_mnr = [[0, 0, 0] for _ in range(len(minerals))] # dia, iron, stone
+
+    for i in range(len(minerals)):
+        if minerals[i] == 'diamond': 
+            cnt_mnr[i//5][0] += 1
+        elif minerals[i] == 'iron': 
+            cnt_mnr[i//5][1] += 1
+        else : 
+            cnt_mnr[i//5][2] += 1
+            
+    # 피로도가 높은 순서대로 광물 정렬
+    cnt_mnr = sorted(cnt_mnr, key = lambda x: (-x[0], -x[1], -x[2]))
+
+    answer = 0
+    for d, i, s in cnt_mnr:
+        for p in range(len(picks)):
+            if p == 0 and picks[p] > 0: # dia 곡괭이
+                picks[p] -=1
+                answer += d + i + s
+                break
+            elif p == 1 and picks[p] > 0: # iron 곡괭이
+                picks[p] -=1
+                answer += 5 * d + i + s
+                break
+            elif p == 2 and picks[p] > 0: # stone 곡괭이
+                picks[p] -=1
+                answer += 25 * d + 5 * i + s
+                break
+                
+    return answer
