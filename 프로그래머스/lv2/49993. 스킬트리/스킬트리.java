e4af5000259a7399c67b396d16cecfad47448686
H <99153215+Hyeon0208@users.noreply.github.com>
2023-05-05 23:31:06 +0900

    [level 2] Title: 스킬트리, Time: 8.96 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for (int i = 0; i < skill_trees.length; i++) {
+            String order = skill_trees[i].replaceAll("[^" + skill + "]", "");
+            String need = skill.substring(0, order.length());
+            if (order.equals(need)) {
+                answer ++;
+            }
+        } 
+        return answer;
+    }
+}
