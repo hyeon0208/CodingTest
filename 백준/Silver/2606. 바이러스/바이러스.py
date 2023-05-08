d08caafb4d7aef2aa7633a5f91c6ebc2b7005c99
H <99153215+Hyeon0208@users.noreply.github.com>
2023-05-08 12:35:56 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,18 @@
 n=int(input()) # 컴퓨터 개수
 v=int(input()) # 연결선 개수
 
-graph = [[] for i in range(n+1)] # 그래프 초기화
-visited=[False] * (n+1) # 방문한 컴퓨터인지 표시
+graph = [[] for _ in range(n + 1)] # 그래프 초기화
+visited=[False] * (n + 1) # 방문한 컴퓨터인지 표시
 
 for i in range(v): # 그래프 생성
     a,b=map(int,input().split())
-    graph[a].append(b) # a에 b 연결
-    graph[b].append(a) # b에 a 연결 -> 양방향
+    graph[a].append(b) 
+    graph[b].append(a) 
 
-def dfs(v):
-    visited[v]=1
-    for nx in graph[v]:
-        if not visited[nx]:
-            dfs(nx)
+def dfs(start):
+    visited[start]=True
+    for next in graph[start]:
+        if not visited[next]:
+            dfs(next)
 dfs(1)
 print(sum(visited)-1)
