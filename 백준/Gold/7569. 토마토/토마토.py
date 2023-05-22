1b01642d70a37cebefa51e642dffdf6e46806b26
H <99153215+Hyeon0208@users.noreply.github.com>
2023-05-22 14:33:40 +0900

    [Gold V] Title: 토마토, Time: 1808 ms, Memory: 48520 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,55 +1,44 @@
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-m, n, h = map(int, input().split())
-
-matrix = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
-visited = [[[False]*m for _ in range(n)] for _ in range(h)]
-
-queue = deque()
+m, n, h = map(int, input().split()) # 열, 행, 상자수
+boxes = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+answer = 0
+q = deque()
 
 dx = [-1,1,0,0,0,0]
 dy = [0,0,-1,1,0,0]
 dz = [0,0,0,0,-1,1]
 
-answer = 0
-
-def bfs():
-    while queue:
-        x,y,z = queue.popleft()
+def bfs(): 
+    while q:
+        box, x, y = q.popleft()
 
         for i in range(6):
             nx = x + dx[i]
             ny = y + dy[i]
-            nz = z + dz[i]
+            nbox = box + dz[i]
 
-            if nx < 0 or nx >= h or ny < 0 or ny >= n or nz < 0 or nz >= m:
-                continue
+            if 0 <= nbox < h and 0 <= nx < n and 0 <= ny < m:
+                if boxes[nbox][nx][ny] == 0:
+                    q.append((nbox, nx, ny))
+                    boxes[nbox][nx][ny] = boxes[box][x][y] + 1
 
-            if matrix[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
-                queue.append((nx,ny,nz))
-                matrix[nx][ny][nz] = matrix[x][y][z] + 1
-                visited[nx][ny][nz] = True
+for box in range(h):
+    for x in range(n):
+        for y in range(m):
+            if boxes[box][x][y] == 1:
+                q.append((box, x, y))
 
-
-# 모두 1이 아닐 경우
-
-for a in range(h):
-    for b in range(n):
-        for c in range(m):
-            if matrix[a][b][c] == 1 and visited[a][b][c] == 0:
-                queue.append((a,b,c))
-                visited[a][b][c] = True
 bfs()
 
-# 토마토 확인
-
-for a in matrix:
-    for b in a:
-        for c in b:
-            if c == 0:
+for box in boxes:
+    for x in box:
+        for y in x:
+            if y == 0:
                 print(-1)
                 exit(0)            
-        answer = max(answer, max(b))
+        answer = max(answer, max(x))
 
 print(answer-1)
