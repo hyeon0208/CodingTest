cb7cd9f04287428371f76946eed7e39dc6735f4c
H <99153215+Hyeon0208@users.noreply.github.com>
2023-07-01 11:11:38 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+import itertools
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort(reverse=True)
+b.sort()
+
+result = 0
+for i in range(n):
+    result += a[i] * b[i]
+
+print(result)
