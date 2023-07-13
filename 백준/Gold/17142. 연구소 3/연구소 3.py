35ce787c64102d364f61e11f683a8c76e0298006
H <99153215+Hyeon0208@users.noreply.github.com>
2023-07-13 17:32:51 +0900

    [Gold III] Title: 연구소 3, Time: 628 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -11,31 +11,31 @@ dy = [-1, 0, 1, 0]
 
 def bfs(virus, blank):
     q = deque()
-    visited = [[-1] * N for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
     time = 0
 
     for i, j in virus:
-        q.append((i, j))
-        visited[i][j] = 0
+        q.append((i, j, 0))
+        visited[i][j] = True
 
     while q:
-        x, y = q.popleft()
-        
+        x, y, cnt = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < N and 0 <= ny < N:
                 # 빈 칸을 만난 경우
-                if laboratory[nx][ny] == 0 and visited[nx][ny] == -1:
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
+                if laboratory[nx][ny] == 0 and not visited[nx][ny]:
+                    q.append((nx, ny, cnt + 1))
+                    visited[nx][ny] = True
                     blank -= 1
-                    time = max(time, visited[nx][ny])
+                    time = max(time, cnt + 1)
                 # 비활성화 바이러스를 만난 경우
-                if laboratory[nx][ny] == 2 and visited[nx][ny] == -1:
-                    q.append((nx, ny))
-                    visited[nx][ny] = visited[x][y] + 1
+                if laboratory[nx][ny] == 2 and not visited[nx][ny]:
+                    q.append((nx, ny, cnt + 1))
+                    visited[nx][ny] = True
     
     if blank != 0:
         return 1e9
