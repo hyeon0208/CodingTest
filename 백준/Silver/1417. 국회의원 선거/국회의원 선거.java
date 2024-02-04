ea0b370c12f829eb7903d942a48e1cc2ad1576e6
H <99153215+Hyeon0208@users.noreply.github.com>
2024-02-04 23:51:37 +0900

    [Silver V] Title: 국회의원 선거, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N <= 1) {
+            System.out.println(0);
+            return;
+        }
+        int dasom = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        while (N-- > 1) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        int result = 0;
+        while (!pq.isEmpty() && pq.peek() >= dasom) {
+            result++;
+            dasom++;
+            pq.add(pq.poll() - 1);
+        }
+        System.out.println(result);
+    }
+}
