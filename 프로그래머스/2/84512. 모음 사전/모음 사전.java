e49fb7f00e1c1f79c53f1630026f4811720a556d
H <99153215+hyeon0208@users.noreply.github.com>
2024-03-16 19:59:27 +0900

    [level 2] Title: 모음 사전, Time: 12.80 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    List<String> words;
+    String[] alpabets = {"A", "E", "I", "O", "U"};
+    
+    public int solution(String word) {
+        words = new ArrayList<>();
+        dfs(word, "", 0);
+        Collections.sort(words);
+        return words.indexOf(word) + 1;
+    }
+    
+    private void dfs(String word, String c, int depth) {
+        if (words.contains(word)) {
+            return;
+        }
+        if (depth == 5) {
+            return;
+        }
+        for (int i = 0; i < alpabets.length; i++) {
+            String newAlpabet = c + alpabets[i];
+            words.add(newAlpabet);
+            dfs(word, newAlpabet, depth + 1);
+        }
+    }
+}
