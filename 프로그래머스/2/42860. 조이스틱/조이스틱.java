0a569c020f044c0b2670e2bc076f816c19ff4aef
H <99153215+hyeon0208@users.noreply.github.com>
2024-04-09 15:04:38 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0; // 조이스틱 조작 횟수
+        int len = name.length();
+        int move = name.length() - 1; // 기본 최소 좌우이동 횟수 (좌, 우 커서)
+        
+        // 해당 커서 알파벳 변경 최솟값 (위, 아래 커서)
+        for (int i = 0; i < len; i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            // 연속된 'A'가 끝나는 지점 찾기
+            int next = i + 1;
+            while(next < len && name.charAt(next) == 'A') {
+                next++;
+            }
+            
+            // 좌우이동 최소 횟수 구하기 (순서대로 가기 vs 뒤로 돌아가기)
+            move = Math.min(move, (i * 2) + len - next);
+            move = Math.min(move, (len - next) * 2 + i);
+        }   
+
+        return answer + move;
+    }
+}
