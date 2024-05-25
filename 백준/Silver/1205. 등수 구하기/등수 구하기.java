96fa39e8d4f85ef9722453bce113dbe24195ff95
H <99153215+hyeon0208@users.noreply.github.com>
2024-05-25 23:29:00 +0900

    [Silver IV] Title: 등수 구하기, Time: 124 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -37,11 +37,7 @@ public class Main {
                 break;
             }
         }
-
-        if (rank == 0) {
-            System.out.println(-1);
-            return;
-        }
+        
         System.out.println(rank);
     }
 }
