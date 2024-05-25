778186dc9b40cb70ecd205bbf4a92bed8c703244
H <99153215+hyeon0208@users.noreply.github.com>
2024-05-25 23:22:03 +0900

    [Silver IV] Title: 등수 구하기, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int taesooScore = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[] score = new int[P];
+        if (N > 0) {
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < N; i++) {
+                score[i] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(score);
+
+        if (P == N && score[0] >= taesooScore) {
+            System.out.println(-1);
+            return;
+        }
+
+        int rank = 1;
+        for (int i = P - 1; i >= Math.max(0, P - N - 1); i--) {
+            if (score[i] > taesooScore) {
+                rank++;
+            } else {
+                break;
+            }
+        }
+
+        if (rank == 0) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(rank);
+    }
+}
