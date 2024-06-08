814129429519de749b4e179e29f98d7e99e7bcb0
H <99153215+hyeon0208@users.noreply.github.com>
2024-06-08 16:45:26 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 804 ms, Memory: 37232 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
@@ -27,25 +23,13 @@ public class Main {
                 wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
             }
         }
-        List<String> words = new ArrayList<>(wordCountMap.keySet());
-        words.sort((o1, o2) -> {
-            int c1 = wordCountMap.get(o1);
-            int c2 = wordCountMap.get(o2);
+        String sortedWords = wordCountMap.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
+                        .thenComparing(Map.Entry.comparingByKey(Comparator.comparingInt(String::length).reversed()))
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining("\n"));
 
-            if (c1 == c2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o2.length() - o1.length();
-            }
-            return c2 - c1;
-        });
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < words.size(); i++) {
-            sb.append(words.get(i)).append("\n");
-        }
-
-        System.out.println(sb);
+        System.out.println(sortedWords);
     }
 }
