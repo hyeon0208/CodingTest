3492ee98a9619050228575fa5015fd72a5e86b41
H <99153215+hyeon0208@users.noreply.github.com>
2024-09-28 20:49:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.77 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+    }
+}
