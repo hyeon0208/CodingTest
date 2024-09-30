dbf162473f3519288dc8cf298d9b772981014cff
H <99153215+hyeon0208@users.noreply.github.com>
2024-09-30 23:39:34 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int carpet = brown + yellow;
+        // 세로의 크기는 3이거나 3보다 커야 노란 격자가 존재할 수 있다.
+        for (int i = 3; i < carpet; i++) {
+            int col = i;
+            int row = carpet / col;
+
+            // 가로의 크기도 3이거나 3보다 커야 노란 격자가 존재할 수 있다.
+            if (row < 3) {
+                continue;
+            }
+
+            if (row >= col) {
+                // 가로, 세로에서 겹치는 부분을 제외하여 곱한 값 = yellow의 넓이
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
