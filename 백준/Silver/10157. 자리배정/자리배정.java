c2d4f92e2ca027eec26236cff114f766f47eece6
H <99153215+hyeon0208@users.noreply.github.com>
2024-11-02 22:50:03 +0900

    [Silver III] Title: 자리배정, Time: 152 ms, Memory: 16900 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int[] DX = {-1, 0, 1, 0};
+    private static final int[] DY = {0, 1, 0, -1};
+
+    private static int C;
+    private static int R;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        if (K > C * R) {
+            System.out.println(0);
+            return;
+        }
+
+        boolean[][] visited = new boolean[C][R];
+        int curX = 0;
+        int curY = 0;
+        int num = 1;
+        int dir = 0;
+        while (num < K) {
+            visited[curX][curY] = true;
+
+            int nx = curX + DX[dir];
+            int ny = curY + DY[dir];
+            if (!isValidRange(nx, ny) || visited[nx][ny]) {
+                dir = (dir + 1) % 4;
+                nx = curX + DX[dir];
+                ny = curY + DY[dir];
+            }
+            curX = nx;
+            curY = ny;
+            num++;
+        }
+
+        System.out.println((curX + 1) + " " + (curY + 1));
+    }
+
+    private static boolean isValidRange(int x, int y) {
+        return 0 <= x && x < C && 0 <= y && y < R;
+    }
+}
