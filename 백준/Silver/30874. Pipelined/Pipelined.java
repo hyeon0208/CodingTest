934b6584f34c579ef10bce286d23592304420753
H <99153215+hyeon0208@users.noreply.github.com>
2025-02-08 01:28:56 +0900

    [Silver II] Title: Pipelined, Time: 536 ms, Memory: 40924 KB -BaekjoonHub

---
+++ 백준/Silver/30874. Pipelined/Pipelined.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] time = new int[N];
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time);
+        System.out.println(time[N - 1] + (N - 1));
+    }
+}
