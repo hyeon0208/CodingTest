135880548a84a869453c33d5cbf41342b6adfe8a
H <99153215+hyeon0208@users.noreply.github.com>
2025-02-15 21:42:24 +0900

    [Gold III] Title: 경사로, Time: 124 ms, Memory: 15212 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,90 +1,128 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, L;
-    static int[][] map;
-    
+
+    private static int N;
+    private static int L;
+    private static int[][] maps;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        
-        map = new int[N][N];
-        
-        // 맵 입력
-        for(int i = 0; i < N; i++) {
+        L = Integer.parseInt(st.nextToken()); // 경사로 길이
+
+        maps = new int[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        int answer = 0;
-        
-        // 가로 방향 확인
-        for(int i = 0; i < N; i++) {
-            if(checkRow(i)) answer++;
+
+        // 길에 속한 모든 칸의 높이가 모두 같아야 한다.
+        // 경사로를 높을 수 있는 데 높이는 항상 1이며 길이는 L이다.
+        // 경사로 높이차이는 1이어야 한다.
+        // 경사로를 놓을 낮은 칸의 높이는 모두 같아야하고 L개의 칸이 연속되어야 한다.
+        // 경사로는 겹칠 수 없다.
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (checkRow(i)) {
+                result++;
+            }
+            if (checkCol(i)) {
+                result++;
+            }
         }
-        
-        // 세로 방향 확인
-        for(int j = 0; j < N; j++) {
-            if(checkCol(j)) answer++;
-        }
-        
-        System.out.println(answer);
+        System.out.println(result);
     }
-    
-    // 가로 방향 확인
-    static boolean checkRow(int row) {
+
+    private static boolean checkRow(int row) {
         boolean[] visited = new boolean[N]; // 경사로 설치 여부 체크
-        
-        for(int i = 1; i < N; i++) {
-            int diff = map[row][i] - map[row][i-1];
-            
-            if(diff == 0) continue; // 높이가 같으면 패스
-            if(Math.abs(diff) > 1) return false; // 높이 차이가 1보다 크면 불가능
-            
-            if(diff == 1) { // 오르막
-                for(int j = 1; j <= L; j++) {
-                    if(i-j < 0 || visited[i-j]) return false;
-                    if(map[row][i-1] != map[row][i-j]) return false;
-                    visited[i-j] = true;
+
+        for (int col = 1; col < N; col++) {
+            int startPoint = maps[row][col - 1];
+            int nextPoint = maps[row][col];
+            int diff = nextPoint - startPoint;
+
+            if (diff == 0) { // 높이가 같으면 패스
+                continue;
+            }
+
+            if (Math.abs(diff) > 1) { // 높이 차이가 1보다 크면 실패
+                return false;
+            }
+
+            if (diff == 1) { // 오르막 (다음지점이 더 클 경우)
+                // 현재 위치(col)에서 왼쪽으로 경사로 길이 L만큼 탐색
+                for (int j = 1; j <= L; j++) {
+                    int nextSlopeIndex = col - j;
+                    if (nextSlopeIndex < 0 || visited[nextSlopeIndex]) { // 범위를 벗어나거나 이미 경사로가 있으면 실패
+                        return false;
+                    }
+                    if (startPoint != maps[row][nextSlopeIndex]) { // 경사로의 높이가 달라지면 실패
+                        return false;
+                    }
+                    visited[nextSlopeIndex] = true;
                 }
-            } else { // 내리막
-                for(int j = 0; j < L; j++) {
-                    if(i+j >= N || visited[i+j]) return false;
-                    if(map[row][i] != map[row][i+j]) return false;
-                    visited[i+j] = true;
+            } else { // 내리막 (시작지점이 더 클 경우)
+                // 현재 위치(col)에서 오른쪽으로 경사로 길이 L만큼 탐색
+                for (int j = 0; j < L; j++) {
+                    int nextSlopeIndex = col + j;
+                    if (nextSlopeIndex >= N || visited[nextSlopeIndex]) {
+                        return false;
+                    }
+                    if (nextPoint != maps[row][nextSlopeIndex]) {
+                        return false;
+                    }
+                    visited[nextSlopeIndex] = true;
                 }
             }
         }
         return true;
     }
-    
-    // 세로 방향 확인
-    static boolean checkCol(int col) {
-        boolean[] visited = new boolean[N]; // 경사로 설치 여부 체크
-        
-        for(int i = 1; i < N; i++) {
-            int diff = map[i][col] - map[i-1][col];
-            
-            if(diff == 0) continue;
-            if(Math.abs(diff) > 1) return false;
-            
-            if(diff == 1) { // 오르막
-                for(int j = 1; j <= L; j++) {
-                    if(i-j < 0 || visited[i-j]) return false;
-                    if(map[i-1][col] != map[i-j][col]) return false;
-                    visited[i-j] = true;
+
+    private static boolean checkCol(int col) {
+        boolean[] visited = new boolean[N];
+
+        for (int row = 1; row < N; row++) {
+            int startPoint = maps[row - 1][col];
+            int nextPoint = maps[row][col];
+            int diff = nextPoint - startPoint;
+
+            if (Math.abs(diff) > 1) {
+                return false;
+            }
+
+            if (diff == 0) {
+                continue;
+            }
+
+            if (diff == 1) { // 오르막
+                for (int j = 1; j <= L; j++) {
+                    int nextSlopeIndex = row - j;
+                    if (nextSlopeIndex < 0 || visited[nextSlopeIndex]) {
+                        return false;
+                    }
+                    if (startPoint != maps[nextSlopeIndex][col]) { // 경사로의 높이가 달라지면 실패
+                        return false;
+                    }
+                    visited[nextSlopeIndex] = true;
                 }
             } else { // 내리막
-                for(int j = 0; j < L; j++) {
-                    if(i+j >= N || visited[i+j]) return false;
-                    if(map[i][col] != map[i+j][col]) return false;
-                    visited[i+j] = true;
+                for (int j = 0; j < L; j++) {
+                    int nextSlopeIndex = row + j;
+                    if (nextSlopeIndex >= N || visited[nextSlopeIndex]) {
+                        return false;
+                    }
+                    if (nextPoint != maps[nextSlopeIndex][col]) {
+                        return false;
+                    }
+                    visited[nextSlopeIndex] = true;
                 }
             }
         }
