e1ea0f4da5143d88ad4b4f5f373778b39ce414ad
H <99153215+hyeon0208@users.noreply.github.com>
2025-04-05 19:42:27 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 81.55 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(String[] gems) {
+        Set<String> uniqueGems = new HashSet<>(Arrays.asList(gems));
+        int totalKinds = uniqueGems.size();
+
+        if (totalKinds == 1) {
+            return new int[]{1, 1};
+        }
+
+       int start = 0;
+        int end = 0;
+        int minLength = gems.length;
+        int resultStart = 0;
+        Map<String, Integer> gemCount = new HashMap<>();
+        boolean found = false;
+        while (end < gems.length) {
+            String currentGem = gems[end];
+            gemCount.put(currentGem, gemCount.getOrDefault(currentGem, 0) + 1);
+            if (gemCount.size() == totalKinds) {
+                found = true;
+                while (start <= end) {
+                    String startGem = gems[start];
+                    if (gemCount.get(startGem) > 1) {
+                        gemCount.put(startGem, gemCount.get(startGem) - 1);
+                        start++;
+                    } else {
+                        break;
+                    }
+                }
+                int curLength = end - start + 1;
+                if (curLength < minLength) {
+                    minLength = curLength;
+                    resultStart = start;
+                }
+            }
+            end++;
+        }
+        if (!found) {
+            return new int[]{1, 1};
+        }
+
+        return new int[]{resultStart + 1, resultStart + minLength};
+    }
+}
