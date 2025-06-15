f3fc57eaef869aa9b6e3ab2736e5036d89b33451
H <99153215+hyeon0208@users.noreply.github.com>
2025-06-15 20:33:29 +0900

    [Silver V] Title: 피보나치 수 4, Time: 160 ms, Memory: 21360 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        } else if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        BigInteger[] fib = new BigInteger[n + 1];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 2].add(fib[i - 1]);
+        }
+
+        System.out.println(fib[n]);
+    }
+}
