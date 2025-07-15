60ff3e717b62ac70c44cf13fc4b0dbfb3189bb85
H <99153215+hyeon0208@users.noreply.github.com>
2025-07-15 18:06:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,12 +1,22 @@
+import java.util.*;
+
 class Solution {
-    public long solution(int n) {
-        long[] dp = new long[2001];
+    public long solution(int n) {        
+        if (n == 1) {
+            return 1;    
+        }
+        if (n == 2) {
+            return 2;
+        }
+        
+        int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-    
-        for (int i = 3; i < 2001; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
+        
         
         return dp[n];
     }
