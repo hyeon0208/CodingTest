d1651fa64d736f1f860e2da630b8cfbb5167a95e
H <99153215+hyeon0208@users.noreply.github.com>
2025-08-01 23:40:47 +0900

    [level 2] Title: 할인 행사, Time: 78.75 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 68.51 ms
+메모리: 116 MB, 시간: 78.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 26일 00:49:39
+2025년 08월 01일 23:40:45
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-// 슬라이딩 윈도우 O(n)
 class Solution {
     final static int DISCOUNT_MATCH_DAY = 10;
     
     public int solution(String[] want, int[] number, String[] discount) {
         int result = 0;
+        
         Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
