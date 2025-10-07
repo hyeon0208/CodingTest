358b4ede183b1fa8f3d3c33d835d4dd2e522ccec
H <99153215+hyeon0208@users.noreply.github.com>
2025-10-07 22:08:04 +0900

    [level 2] Title: 할인 행사, Time: 43.21 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,34 +1,48 @@
 import java.util.*;
 
 class Solution {
-    final static int DISCOUNT_MATCH_DAY = 10;
+    private final static int MEMBERSHIP_VLAID_DAY = 10;
     
     public int solution(String[] want, int[] number, String[] discount) {
         int result = 0;
-        
+            
         Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
-        
-        for (int i = 0; i < discount.length - DISCOUNT_MATCH_DAY + 1; i++) {
-            Map<String, Integer> buyMap = new HashMap<>();
-            for (int j = i; j < i + DISCOUNT_MATCH_DAY; j++) {
-                buyMap.put(discount[j], buyMap.getOrDefault(discount[j], 0) + 1);
-            }
-            
-            boolean allMatch = true;
-            for (String item : want) {
-                if (!buyMap.getOrDefault(item, 0).equals(wantMap.get(item))) {
-                    allMatch = false;
-                }
-            }
-            
-            if (allMatch) {
-                result++;
+
+        for (int i = 0; i < MEMBERSHIP_VLAID_DAY; i++) {
+            if (wantMap.containsKey(discount[i])) {
+                wantMap.put(discount[i], wantMap.get(discount[i]) - 1);
             }
         }
-                
+        
+        if (allMatch(wantMap)) {
+            result++;
+        }
+        
+        for (int i = MEMBERSHIP_VLAID_DAY; i < discount.length; i++) {
+            if (wantMap.containsKey(discount[i - MEMBERSHIP_VLAID_DAY])) {
+                wantMap.put(discount[i - MEMBERSHIP_VLAID_DAY], wantMap.get(discount[i - MEMBERSHIP_VLAID_DAY]) + 1);
+            }
+            
+            if (wantMap.containsKey(discount[i])) {
+                wantMap.put(discount[i], wantMap.get(discount[i]) - 1);
+            }
+            
+            if (allMatch(wantMap)) {
+                result++;
+            }   
+        }
         return result;
+    }
+    
+    private boolean allMatch(Map<String, Integer> map) {
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
