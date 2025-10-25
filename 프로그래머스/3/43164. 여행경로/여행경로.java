75c26278a50a1a359230726bc2b4f4f734c47c68
H <99153215+hyeon0208@users.noreply.github.com>
2025-10-26 02:21:38 +0900

    [level 3] Title: 여행경로, Time: 77.53 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -5,40 +5,24 @@ class Solution {
     private static List<String> results;
     
     public static String[] solution(String[][] tickets) {
-        Map<String, Integer> map = new HashMap<>();
-        for (String[] ticket : tickets) {
-            map.put(ticket[0], map.getOrDefault(ticket[0], 0) + 1);
-            map.put(ticket[1], map.getOrDefault(ticket[1], 0) + 1);
-        }
-
-        results = new ArrayList<>();
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("ICN").append(" ");
         boolean[] visited = new boolean[tickets.length];
-        backTracking(0, tickets, visited, sb, map, "ICN");
-        Collections.sort(results);            
-
+        results = new ArrayList<>();
+        dfs("ICN", "ICN", 0, tickets, visited);
+        Collections.sort(results);
         return results.get(0).split(" ");
     }
 
-    private static void backTracking(int depth, String[][] tickets, boolean[] visited, StringBuilder sb, Map<String, Integer> map, String prev) {
+    private static void dfs(String current, String path, int depth, String[][] tickets, boolean[] visited) {
         if (depth == tickets.length) {
-            results.add(sb.toString());
+            results.add(path);
             return;
         }
-
+        
         for (int i = 0; i < tickets.length; i++) {
-            String[] ticket = tickets[i];
-            if (prev.equals(ticket[0]) && !visited[i]) {
-                map.put(ticket[0], map.getOrDefault(ticket[0], 0) - 1);
-                map.put(ticket[1], map.getOrDefault(ticket[1], 0) - 1);
-                sb.append(ticket[1]).append(" ");
+            if (current.equals(tickets[i][0]) && !visited[i]) {
                 visited[i] = true;
-                backTracking(depth + 1, tickets, visited, sb, map, ticket[1]);
+                dfs(tickets[i][1], path + " " + tickets[i][1], depth + 1, tickets, visited);
                 visited[i] = false;
-                int len = ticket[1].length();
-                sb.delete(sb.length() - len - 1, sb.length());
             }
         }
     }
